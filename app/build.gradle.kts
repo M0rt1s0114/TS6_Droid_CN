771bd284290986123372e9ca65f3419383e3b82a
Mortis0114 <46196322+M0rt1s0114@users.noreply.github.com>
2026-08-19 05:39:58 +0800

    chore(release): bump version to 2.1.3-4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yuaxi.ts6droid.cn"
         minSdk = 29
         targetSdk = 36
-        versionCode = 15
-        versionName = "2.1.3-3"
+        versionCode = 16
+        versionName = "2.1.3-4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
